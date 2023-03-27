88f9ad3c731852ef8da156131693b131bcc45e4a
Bartolemy12 <127960590+Bartolemy12@users.noreply.github.com>
2023-03-28 00:48:32 +0300

    Update Enemy.gd
    Исправлены ошибки, добавлены функции.

--- Enemies/Enemy.gd
+++ Enemies/Enemy.gd
@@ -1,21 +1,14 @@
+class_name EnemyUnit
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
+func _init():
+	pass 
+	#add_to_group("")
+	
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	move_and_slide()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -25,4 +18,15 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+func search_route(): 
+	pass
+	
+func update_animation():
+	pass
+	
+func _ready():
+	pass
+	
+func scream_from_damage():
+	pass
+	
