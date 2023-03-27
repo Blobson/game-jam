0b03e6bd17a3fec5395e9250e8c314a52b64bb97
Svedmit <127945929+Svedmit@users.noreply.github.com>
2023-03-27 14:08:50 +0700

    #22 Главное меню игры, #22 Настройки
    Исправление ошибок + добавил паузу

---
+++ ui/MainMenu/main_menu.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var gamesaved: bool = 0 #наличие сохранений в игре. Когда игрок создаст сохранение, должна поменяться в 1.
+
+func _ready():
+	$CanvasLayer/Buttons/ContinueButton.grab_focus() #Подсвечена кнопка 'продолжить' при запуске
+	game_saved_test()
+
+
+func game_saved_test(): #проверка наличия сохранений
+	if not gamesaved:
+		$CanvasLayer/Buttons/ContinueButton.visible = false 
+		$CanvasLayer/Buttons/NewGameButton.grab_focus()
+	else:
+		$CanvasLayer/Buttons/ContinueButton.visible = true		
+
+
+func _on_continue_button_pressed():
+	get_tree().change_scene_to_file("saved level") #запуск сохраненного уровня
+
+
+func _on_new_game_button_pressed():
+	get_tree().change_scene_to_file("level1") #запуск 1го уровня
+
+
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://ui/SettingsMenu/settings.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
--- ui/SettingsMenu/settings.gd
+++ ui/SettingsMenu/settings.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+func _ready():
+	$SettingsElements/BackToMenuButton.grab_focus()
 
 func _on_music_slider_value_changed(value):
 	var bus_index = AudioServer.get_bus_index("Music")
@@ -19,3 +21,7 @@ func _on_sfx_slider_value_changed(value):
 	else:
 		AudioServer.set_bus_mute(bus_index, true)	
 
+
+
+func _on_back_to_menu_button_pressed():
+	get_tree().change_scene_to_file("res://ui/MainMenu/main_menu.tscn")
