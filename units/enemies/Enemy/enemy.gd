8c74273bcce2d3ac6e5353cb995fc58ce959d934
DemitrSpb <38684545+DemitrSpb@users.noreply.github.com>
2023-04-17 23:26:12 +0300

    #48 Add HP bar (#60)
    #48 Add HP bar

--- units/enemies/Enemy/enemy.gd
+++ units/enemies/Enemy/enemy.gd
@@ -1,24 +1,42 @@
 class_name Enemy extends CharacterBody2D
 
+## Скорость движения моба
 @export var move_speed: int = 150
+## Здоровье моба
 @export var health: int = 50
+## Сила атаки моба
 @export var attack_power: int = 10
+## Смещение полоски с ХП по y
+@export var health_bar_offset: int = -20
+
 var path_follower: EnemyPathFollower
+
+@onready var hp_progress = $HealthBar/HpProgressBar
+@onready var sprite = $Sprite2D
 
 func _init():
 	add_to_group("enemies")
 
+# Текст с дебаг инфо по мобу
+func _print_info():
+	$Label.text = """
+	name: %s
+	health: %s
+	""" % [name, health]
 
 func _ready():
-	$HealthBar.set("max_value", health)
-	$HealthBar.set("value", health)
+	# настройка HP-бара
+	$HealthBar.position = Vector2(0, health_bar_offset)
+	hp_progress.max_value = health
+	hp_progress.step = health / 5
+	hp_progress.value = health
+	_print_info()
 	
 ## Установить новый EnemyPathFollower
 func set_path_follower(path_follower: EnemyPathFollower):
 	self.path_follower = path_follower
 	position = path_follower.get_position()
 	$Sprite2D.set("flip_h", path_follower.get_direction().x < 0)
-
 
 ## Перемещение
 func _move(delta: float):
@@ -31,7 +49,12 @@ func _move(delta: float):
 ## Получение урона
 func take_damage(damage_count):
 	health -= damage_count
-	$HealthBar.set("value", health)
+	hp_progress.value = health if health > 0  else 0
+
+	# TODO: remove debug
+	print_debug("%s TAKE dmg=%s, health=%s" % [name, damage_count, health])
+	_print_info()
+	
 	if health <= 0:
 		queue_free() ##Смерть от получения урона
 
