409c879025361bd608135a3410a2183416a6850e
DemitrSpb <demitr@gmail.com>
2023-04-26 23:42:05 +0300

    #53 Refactor BuildMenu

--- game.gd
+++ game.gd
@@ -1,58 +1,11 @@
 extends Node2D
 
-var tower_build_menu
-var current_build_location
-
 @onready var current_level = get_node("/root/game/level").get_child(0)
-
-var towers = {
-	"tower_1": preload("res://towers/TowerSaw/tower_saw.tscn"),
-	"tower_2": preload("res://towers/CatapultTower/catapult_tower.tscn")
-}
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#устанавливаем позицию камеры по центру картинки заднего плана
 	$Camera.position = current_level.get_node("Background").position
-	tower_build_menu = load("res://ui/TowerBuildMenu/tower_build_menu.tscn").instantiate()
 	$BackgroundMusic.play()
-	
-#	current_level.print_tree_pretty()
 
-	# прописать обработку нажатий на TowerBuildLocation
-	for child in current_level.get_children():
-		if child is TowerBuildLocation:
-			child.TowerBuildLocationClick.connect(_on_TowerBuildLocationClick)
-
-
-func _on_TowerBuildLocationClick(build_location):
-	current_build_location = build_location
-	current_level.remove_child(build_location)
-
-	# connect signals
-	tower_build_menu.BuildMenu_Close.connect(_on_BuildMenu_Close)
-	tower_build_menu.BuildMenu_TowerSelected.connect(_on_BuildMenu_TowerSelected)
-
-	# show menu
-	tower_build_menu.position = build_location.position
-	current_level.add_child(tower_build_menu)
-
-
-# Обработка выбора башни в меню постройки
-func _on_BuildMenu_TowerSelected(tower):
-	print_debug("Handle BuildMenu_TowerSelected(): tower=" + tower)
-	current_level.remove_child(tower_build_menu)
-	if tower in towers:
-		var twr = towers[tower].instantiate()
-		twr.position = tower_build_menu.position
-		add_child(twr)
-	else:
-		print("Tower '%s' not found".format(tower))
-
-
-# Обработка закрытия меню постройки башни без выбора башни
-func _on_BuildMenu_Close():
-	print_debug("Handle BuildMenu_Close()")
-	current_level.remove_child(tower_build_menu)
-	current_level.add_child(current_build_location)
--- utils/TowerBuildLocation/tower_build_location.gd
+++ utils/TowerBuildLocation/tower_build_location.gd
@@ -1,7 +1,22 @@
 class_name TowerBuildLocation extends Node2D
 
-signal TowerBuildLocationClick
+@onready var build_menu = get_node("/root/game/ui/TowerBuildMenu")
 
 
+# Установка башни на месте BuildLocation
+func put_tower(twr):
+		twr.position = position
+
+		var parent = get_parent()
+		var pos_in_parent = get_index()
+		
+		parent.remove_child(self)
+		parent.add_child(twr)
+		parent.move_child(twr, pos_in_parent)
+
+		call_deferred("free")
+
+# Нажатие на Location
 func _on_touch_screen_button_pressed():
-	emit_signal("TowerBuildLocationClick", self)
+	build_menu.show_menu(self)
+	
