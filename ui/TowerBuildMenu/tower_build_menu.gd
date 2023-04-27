4f86a09b0081f37ed001fc747f1f19fb8c595a7a
DemitrSpb <38684545+DemitrSpb@users.noreply.github.com>
2023-04-28 00:49:00 +0300

    #53 Refactor BuildMenu (#84)

--- ui/TowerBuildMenu/tower_build_menu.gd
+++ ui/TowerBuildMenu/tower_build_menu.gd
@@ -1,9 +1,43 @@
 extends Node2D
 
-# Сигнал выбора башни (нажата кнопка башни)
-signal BuildMenu_TowerSelected
-# Сигнал нажатия куда либо (т.е. должно закрыться меню)
-signal BuildMenu_Close
+var towers = {
+	"tower_1": preload("res://towers/TowerSaw/tower_saw.tscn"),
+	"tower_2": preload("res://towers/CatapultTower/catapult_tower.tscn")
+}
+
+var build_location: TowerBuildLocation = null
+
+
+func _ready():
+	# выключаем обработку ивентов пака меню не активно
+	set_process_input(false)
+
+
+# Показ меню для указанного build location
+func show_menu(b: TowerBuildLocation):
+	# включаем обработку ивентов
+	set_process_input(true)
+	position = b.position
+	build_location = b
+	show()
+
+
+# Обработка выбора башни в меню постройки
+func put_tower(tower):
+	print_debug("BuildMenu:put_tower(): tower=" + tower)
+	
+	if tower in towers:
+		var twr = towers[tower].instantiate()
+		build_location.put_tower(twr)
+	else:
+		print("Tower '%s' not found".format(tower))
+
+
+# Обработка закрытия меню постройки башни без выбора башни
+func close_menu():
+	print_debug("BuildMenu:close_menu()")
+	self.hide()
+	set_process_input(false)
 
 
 # У TouchScreenButton должен быть прописан action с названием башни
@@ -11,13 +45,8 @@ signal BuildMenu_Close
 #  - InputEventAction с атрибутом action
 #  - InputEventScreenTouch что кнопка нажата\отжата
 func _input(event):
-	# print(event)
-	# print_tree_pretty()
-	
 	if event is InputEventAction && !event.is_pressed():
-		BuildMenu_TowerSelected.emit(event.action)
-		# var p = get_parent()
-		# get_node("/root").set_input_as_handled()
-				
+		put_tower(event.action)
+
 	if event is InputEventScreenTouch && !event.is_pressed():
-		BuildMenu_Close.emit()
+		close_menu()
