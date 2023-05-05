021a139fdb282ea04902ad48323b429aaef5cdbf
Svedmit <127945929+Svedmit@users.noreply.github.com>
2023-05-06 01:22:28 +0700

    #92 Индикатор доступного золота

--- ui/GoldIndicator/gold_indicator.gd
+++ ui/GoldIndicator/gold_indicator.gd
@@ -1,9 +1,26 @@
 class_name GoldIndicator extends Control
 
-@onready var gold_indicator_value: int
+#Время анимации tween
+@onready var tween_duration: float = 0.8
+
 
 func _ready():
-	gold_indicator_value = $"/root/Game".gold
-	
+	#Подсоединяем сигнал change_value
+	var game = get_node("/root/Game")
+	Game.gold_changed.connect(change_value)
+
+
 func change_value(old, new):
-	$HBoxContainer/GoldValue.text = str(new)
+	#создаем tween, задаем параметры 
+	var tween_indicator = create_tween()
+	tween_indicator.set_trans(Tween.TRANS_QUAD)
+	tween_indicator.set_ease(Tween.EASE_IN_OUT)
+	#Запускаем tween, передаем значение в counert_interpolate
+	tween_indicator.tween_method(counter_interpolate, old, new, tween_duration)
+
+
+func counter_interpolate(gold_value):
+	#Округляем значение
+	var round_gold_value = round(gold_value)
+	#Отображаем на индикаторе
+	$HBoxContainer/GoldCounter.text = str(round_gold_value)
