c397dfd93f26ef72d3827b8ac113b973c19540ac
Svedmit <127945929+Svedmit@users.noreply.github.com>
2023-05-29 17:21:48 +0700

    #126 Эффект взрыва камней
    Добавил эффект взрыва камней, изменил цвет на песчаный, поправил механику взрыва

--- utils/Explosion/explosion.gd
+++ utils/Explosion/explosion.gd
@@ -2,11 +2,11 @@ class_name Explosion extends Node2D
 
 
 func _ready():
-	$AnimationPlayer.play("explosion")
+	$ExplosionSprite.play("Explosion") #Важно дать название анимации - Explosion
 	$ExplosionSFX.pitch_scale = randf_range(0.8, 1.2)
 	$ExplosionSFX.play()
 
 
-func _on_animation_player_animation_finished(explosion):
+#Удаляем ноду по окончании проирывания звука
+func _on_explosion_sfx_finished():
 	queue_free()
-	
